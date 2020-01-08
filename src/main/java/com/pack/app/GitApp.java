a3531e3e5764db36fa9e9c3a184c2ae377e68fdd
afiregit <afire.lee@gmail.com>
2020-01-08 10:34:41 +0800

    010803_edit

--- src/main/java/com/pack/app/GitApp.java
+++ src/main/java/com/pack/app/GitApp.java
@@ -5,6 +5,8 @@ public class GitApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println( "Hello World!" );
+		 System.out.println( "Hello GIT!" );
+		 
 	}
 
 }
